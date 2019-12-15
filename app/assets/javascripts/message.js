61b6f38f40f4422b85d46afa6d9b7bfa78a188a4
miyat-yuka <58365099+miyat-yuka@users.noreply.github.com>
2019-12-15 18:17:45 +0900

    修正しました。

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -3,38 +3,38 @@ $(function(){
   function buildHTML(message){
     if (message.image) {
       var html = `<div class="message">
-                  <div class="upper-message">
-                  <div class="upper-message__user-name">
-                    ${message.user_name}
+                    <div class="upper-message">
+                      <div class="upper-message__user-name">
+                      ${message.user_name}
+                      </div>
+                      <div class="upper-message__date">
+                      ${message.created_at}
+                    </div>
                   </div>
-                  <div class="upper-message__date">
-                    ${message.created_at}
-                  </div>
-                  </div>
-                  <div class="lower-message">
+                <div class="lower-message">
                   <p class="lower-message__content">
-                    ${message.content}
+                  ${message.content}
                   </p>
-                  </div>
-                   ${message.image}
-                  </div>`
+                </div>
+                ${message.image}
+                </div>`
     } else {
       var html = `<div class="message">
-                  <div class="upper-message">
-                  <div class="upper-message__user-name">
-                    ${message.user_name}
+                    <div class="upper-message">
+                      <div class="upper-message__user-name">
+                      ${message.user_name}
+                      </div>
+                      <div class="upper-message__date">
+                      ${message.created_at}
+                    </div>
                   </div>
-                  <div class="upper-message__date">
-                    ${message.created_at}
-                  </div>
-                  </div>
-                  <div class="lower-message">
+                <div class="lower-message">
                   <p class="lower-message__content">
-                    ${message.content}
+                  ${message.content}
                   </p>
                   
-                  </div>
-                  </div>`
+                </div>
+                </div>`
     }
     return html
   }
