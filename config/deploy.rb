17502796c444d81c00b91ba3c24ec80b4c58e302
miyat-yuka <58365099+miyat-yuka@users.noreply.github.com>
2019-12-19 21:27:28 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 lock "~> 3.11.2"
 
 set :application, 'chat-space'
-set :repo_url, 'git@example.com:miyat-yuka/chat-space.git'
+set :repo_url, 'git@github.com:miyat-yuka/chat-space.git'
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 set :rbenv_type, :user
